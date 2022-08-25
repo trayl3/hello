9e4e46257d040b1372cba5074da89baa8f4b9d68
trayl3 <111883852+trayl3@users.noreply.github.com>
2022-08-25 20:08:07 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Tyler Rayl, Mason Filter
+ *         trayl3@huskers.unl.edu
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Tyler Rayl and Mason Filter\n");
+
+  return 0;
+}
